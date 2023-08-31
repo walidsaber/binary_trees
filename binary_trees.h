fa7624c8a3af5d8619d8b84291d1732f27bc4883
walidsaber <walid_saber123@outlook.com>
2023-08-31 10:22:54 -0700

    4-binary_tree_is_leaf.c

--- binary_trees.h
+++ binary_trees.h
@@ -31,5 +31,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* task 3 */
 void binary_tree_delete(binary_tree_t *tree);
+/* task 4 */
+int binary_tree_is_leaf(const binary_tree_t *node);
+/* task 5 */
+
 #endif
 
