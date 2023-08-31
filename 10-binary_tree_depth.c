975d12675e1c14e1bfb3ee25c1a8e035245f1d3a
walidsaber <walid_saber123@outlook.com>
2023-08-31 11:33:12 -0700

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - depth of tree
+ * @tree: node
+ * Return: depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
