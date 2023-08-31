27af17ecc6f97e49c8e861d7c89d5019ea24e6e4
walidsaber <walid_saber123@outlook.com>
2023-08-31 06:47:27 -0700

    1-binary_tree_insert_left.c

--- binary_trees.h
+++ binary_trees.h
@@ -22,10 +22,10 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-/* task 1 */
+/* task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
-/* task 2 */
+/* task 1 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif
 
