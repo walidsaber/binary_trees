f79cf26ebcf2583ddcf518a53b1290d44c181998
walidsaber <walid_saber123@outlook.com>
2023-08-31 09:58:21 -0700

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - delete the tree
+ * @tree: tree
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -30,5 +30,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 /* task 2 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* task 3 */
+void binary_tree_delete(binary_tree_t *tree);
 #endif
 
