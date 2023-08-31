64acb233f0eaff7275c32efdbaf1a8e33fa8f8cf
walidsaber <walid_saber123@outlook.com>
2023-08-31 06:34:41 -0700

    0-

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,10 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
-
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,4 +24,5 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 /* task 1 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
 #endif
