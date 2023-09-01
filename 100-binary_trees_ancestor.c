179c98ec7f572091463fbb6fabab40b397c2a466
walidsaber <walid_saber123@outlook.com>
2023-09-01 07:00:29 -0700

    100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - ancestor
+ * @first: node1
+ * @second: node2
+ * Return: ancestor
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *p, *q;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+	p = first->parent;
+	q = second->parent;
+	if (p == NULL || first == q || (!p->parent && q))
+		return (binary_trees_ancestor(first, q));
+	else if (q == NULL || p == second || (!q->parent && p))
+		return (binary_trees_ancestor(p, second));
+	return (binary_trees_ancestor(p, q));
+}
