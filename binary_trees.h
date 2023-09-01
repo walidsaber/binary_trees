33bd6ec8a4bd1356341833b3ba19ae80fc9ba7a6
walidsaber <walid_saber123@outlook.com>
2023-09-01 07:48:43 -0700

    1

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,16 @@ typedef struct link_s
 	struct binary_tree_s const *node;
 	struct link_s *next;
 } link_t;
+/**
+ * struct levelorder_queue_s - Level order
+ * @node: A node
+ * @next: The next node
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
 /* task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
