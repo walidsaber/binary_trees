7e908ae4d5908d596d54656ba904b84721abc4f0
walidsaber <walid_saber123@outlook.com>
2023-09-01 07:21:14 -0700

    101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -29,6 +29,36 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
 /**
+ * linked_node - linked node
+ * @head: pointer
+ * @tree: node
+ * @level: depth
+ * Return: v
+ */
+void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
+{
+	link_t *new, *aux;
+
+	new = malloc(sizeof(link_t));
+	if (new == NULL)
+		return;
+	new->n = level;
+	new->node = tree;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		aux = *head;
+		while (aux->next != NULL)
+			aux = aux->next;
+		new->next = NULL;
+		aux->next = new;
+	}
+}
+/**
  * recursion - loop
  * @head: pointer
  * @tree: node
