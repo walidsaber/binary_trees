eda61ddfa2ad01a7896b3774848c26ee6a80efcb
walidsaber <walid_saber123@outlook.com>
2023-09-01 07:23:16 -0700

    101-

--- binary_trees.h
+++ binary_trees.h
@@ -25,6 +25,19 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+/*  linked list for advanced 101 the levelorder */
+/**
+ * struct link_s - structure
+ * @n: num
+ * @node: node
+ * @next: next node
+ */
+typedef struct link_s
+{
+	size_t n;
+	struct binary_tree_s const *node;
+	struct link_s *next;
+} link_t;
 /* task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
