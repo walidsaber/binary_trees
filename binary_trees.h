cc654b11208fa898f19f320efe42aba332e7441d
walidsaber <walid_saber123@outlook.com>
2023-09-01 07:13:42 -0700

    100-

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 /* task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
