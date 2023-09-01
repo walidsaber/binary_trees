f05ee8b67a9b97ba6d6e88a88d22f3afb007fbe6
walidsaber <walid_saber123@outlook.com>
2023-09-01 07:19:50 -0700

    101-binary_tree_levelorder.c

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,81 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - how tall are u tree?
+ * @tree: tree
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree)
+	{
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	}
+	return ((left > right) ? left : right);
+}
+/**
+ * binary_tree_depth - depth of tree
+ * @tree: node
+ * Return: depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
+/**
+ * recursion - loop
+ * @head: pointer
+ * @tree: node
+ * Return: v
+ */
+void recursion(link_t **head, const binary_tree_t *tree)
+{
+	size_t level = 0;
+
+	if (tree == NULL)
+		return;
+	level = binary_tree_depth(tree);
+	linked_node(head, tree, level);
+	recursion(head, tree->left);
+	recursion(head, tree->right);
+}
+/**
+ * binary_tree_levelorder - order
+ * @tree: root
+ * @func: function
+ * Return: v
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	link_t *head, *aux;
+	size_t height = 0, count = 0;
+
+	if (!tree || !func)
+		return;
+	height = binary_tree_height(tree);
+	head = NULL;
+	recursion(&head, tree);
+	while (count <= height)
+	{
+		aux = head;
+		while (aux != NULL)
+		{
+			if (count == aux->n)
+				func(aux->node->n);
+			aux = aux->next;
+		}
+		count++;
+	}
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+}
